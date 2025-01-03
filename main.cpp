aebfd3245960cd5839b9876dde935793e5845696
ChrisCoolman <159795512+ChrisCoolman@users.noreply.github.com>
2025-01-03 08:58:40 -0500

    added check to make sure it is actually playboi carti and not a fake

--- main.cpp
+++ main.cpp
@@ -7,20 +7,31 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     string password = "iAmNeverDroppingAgain123";
     string guess;
-    cout << "Welcome!" << endl; 
+    cout << "Welcome!" << endl;
     cout << "Please enter the password: " << endl;
     cin >> guess;
-    if(guess == password)
+    if (guess == password)
     {
         cout << "Welcome Playboi Carti!" << endl;
-        cout << "Would you like to drop music?" << endl;
+        cout << "Would you like to drop music? y/n" << endl;
+        string answer;
+        cin >> answer;
+        if(answer == "y")
+        {
+            cout << "You are not Playboi Carti!" << endl;
+        }
+        else
+        {
+            cout << "Welcome Playboi Carti!" << endl;
+            // put the real program here!!!!!
+        }
     }
     else
     {
         cout << "You are not Playboi Carti!" << endl;
     }
-
 }
