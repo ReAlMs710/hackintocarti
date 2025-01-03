529d4fc67d1bf09ad3a4e80cb7310aec807f2b12
ChrisCoolman <159795512+ChrisCoolman@users.noreply.github.com>
2025-01-03 08:43:28 -0500

    Added header

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+// Chris, Ryan, Neil
+// 1/3/2025
+// Hack Into Carti
+// Extra: TBD
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "Hello" << endl; 
+    cout << "Welcome!" << endl; 
 }
