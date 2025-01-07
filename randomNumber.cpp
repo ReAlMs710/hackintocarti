20b3bdc87b785f37272744fbb9179c030ffa50a1
ChrisCoolman <159795512+ChrisCoolman@users.noreply.github.com>
2025-01-07 08:36:43 -0500

    Added std

--- randomNumber.cpp
+++ randomNumber.cpp
@@ -2,30 +2,30 @@
 #include <iostream>
 #include <stdio.h>
 #include <cstdlib>
-using namespace std;
+
 int main() {
     /* initialize random seed: */
     srand (time(NULL));
 
-    cout << "How many songs will be on the album?" << endl;
-    cout << "Guess the number between 10 - 30" << endl;
+    std::cout << "How many songs will be on the album?" << std::endl;
+    std::cout << "Guess the number between 10 - 30" << std::endl;
     int tries = 0;
     int guess = 0;
     int songs = rand() % 30 + 10;
     do {
         if(tries > 0 && guess < songs) {
-            cout << "Guess a higher number" << endl;
+            std::cout << "Guess a higher number" << std::endl;
         }
         if(tries > 0 && guess > songs) {
-            cout << "Guess a lower number" << endl;
+            std::cout << "Guess a lower number" << std::endl;
         }
         // Tell them how many tries after first try
         if(tries > 0) {
-          cout << "This is your " << tries << " attempt" << endl;   
+          std::cout << "This is your " << tries << " attempt" << std::endl;   
         }
-        cout << "Please enter your guess" << endl;
-        cin >> guess;
+        std::cout << "Please enter your guess" << std::endl;
+        std::cin >> guess;
         tries += 1;
     } while(guess != songs); // condition after loop
-    cout << "CORRECT! There are " << songs << " songs on the album which took you " << tries << " tries to guess!" << endl;
+    std::cout << "CORRECT! There are " << songs << " songs on the album which took you " << tries << " tries to guess!" << std::endl;
 }
